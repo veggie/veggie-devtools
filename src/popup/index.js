2d97e9f74650a8909aec45bfd8a683ae805d44af
Mickey Burks <brks.mck@gmail.com>
2018-01-14 00:19:19 -0600

    Build popup to use common getTab function

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,12 +1,4 @@
-function getTab () {
-  return new Promise((resolve, reject) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const [ tab ] = tabs
-      console.log(tab)
-      resolve(tab)
-    })
-  })
-}
+import { getTab } from '../common'
 
 ;(async function () {
   const tab = await getTab()
