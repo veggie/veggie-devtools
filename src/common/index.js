ae9aa34d923fa1647bcb95015fd2f3a708c6502a
Mickey Burks <mickey@burks.online>
2018-01-23 01:23:07 -0600

    Use veggie v3.0.0 beta

--- src/common/index.js
+++
@@ -1,9 +0,0 @@
-export function getTab () {
-  return new Promise((resolve, reject) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const [ tab ] = tabs
-      console.log(tab)
-      resolve(tab)
-    })
-  })
-}
