a4124534d0bfe67454509dc45f5d7d8addb640e8
Mickey Burks <brks.mck@gmail.com>
2018-05-13 11:20:38 -0500

    Remove vue and old components

---
+++ src/panel.js
@@ -0,0 +1,4 @@
+import initApp from './app'
+import './app/components/VeggiePanel/VeggiePanel.tag'
+
+initApp('veggie-panel')
--- src/panel/config.js
+++
@@ -1,6 +0,0 @@
-export const chipColors = {
-  block: 'red',
-  hang: 'yellow',
-  set: 'blue',
-  status: 'green'
-}
